3c9c268e559ea853d81a75f37a556ac39d480c66
shiiman <hsnonsense5@gmail.com>
2022-06-03 17:44:15 +0900

    add instance

---
+++ lesson3/3. bastion/ec2.tf
@@ -0,0 +1,21 @@
+/**
+ * インスタンス作成
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+ */
+resource "aws_instance" "bastion" {
+  count         = 1
+  ami           = "ami-02c3627b04781eada"
+  instance_type = "t2.micro"
+  key_name      = "instance_access_key"
+  #vpc_security_group_ids      = var.vpc_security_group_ids
+  subnet_id                   = resource.aws_subnet.subnet_public.*.id[count.index % length(resource.aws_subnet.subnet_public.*.id)]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = format("${var.user_name}-bastion%04d", count.index + 1)
+  }
+
+  volume_tags = {
+    Name = "${format("${var.user_name}-bastion%04d", count.index + 1)}-ebs"
+  }
+}
---
+++ lesson3/3. bastion/terraform.tfvars
@@ -0,0 +1,5 @@
+user_name = "shiiman"
+
+// subnet
+availability_zones        = ["ap-northeast-1a", "ap-northeast-1c"]
+subnet_public_cidr_blocks = ["172.16.0.0/24", "172.16.1.0/24"]
