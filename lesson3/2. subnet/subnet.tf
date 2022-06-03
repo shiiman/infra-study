c38d0818fca49b90164f5daacb4bfcb102c737d4
shiiman <hsnonsense5@gmail.com>
2022-06-03 17:12:39 +0900

    update subnet

--- lesson3/2. subnet/subnet.tf
+++ lesson3/2. subnet/subnet.tf
@@ -1,12 +1,15 @@
+variable "availability_zones" { type = list(string) }
+variable "subnet_public_cidr_blocks" { type = list(string) }
+
 /**
  * subnet作成
  * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
  */
 resource "aws_subnet" "subnet_public" {
-  count                   = 2
+  count                   = length(var.subnet_public_cidr_blocks)
   vpc_id                  = resource.aws_vpc.vpc.id
-  cidr_block              = ["172.16.0.0/24", "172.16.1.0/24"]
-  availability_zone       = ["ap-northeast-1a", "ap-northeast-1c"]
+  availability_zone       = var.availability_zones[count.index % length(var.availability_zones)]
+  cidr_block              = var.subnet_public_cidr_blocks[count.index]
 
   tags = {
     Name = format("${var.user_name}-public-%01d-subnet", count.index + 1)
