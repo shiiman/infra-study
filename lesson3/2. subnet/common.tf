adc72d6202c23a5829f3ae94c76fe5185573da20
shiiman <hsnonsense5@gmail.com>
2022-06-03 16:53:07 +0900

    update common

--- lesson3/2. subnet/common.tf
+++ lesson3/2. subnet/common.tf
@@ -1,16 +1,17 @@
 terraform {
-    required_version = ">= 1.1.9"
-
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "4.10.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
     }
+  }
+
+  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
-    region = "ap-northeast-1"
+  profile = "default"
+  region  = "ap-northeast-1"
 }
 
 variable "user_name" { type = string }
