e1a9ce7d33009709c37b0afeda4c2a680db7e17c
shiiman <hsnonsense5@gmail.com>
2022-06-03 18:15:05 +0900

    update subnet

--- lesson3/2. subnet/terraform.tfvars
+++ lesson3/2. subnet/terraform.tfvars
@@ -1,5 +1,6 @@
 user_name = "shiiman"
 
 // subnet
-availability_zones        = ["ap-northeast-1a", "ap-northeast-1c"]
-subnet_public_cidr_blocks = ["172.16.0.0/24", "172.16.1.0/24"]
+availability_zones         = ["ap-northeast-1a", "ap-northeast-1c"]
+subnet_public_cidr_blocks  = ["172.16.0.0/24", "172.16.1.0/24"]
+subnet_private_cidr_blocks = ["172.16.10.0/24", "172.16.11.0/24"]
--- lesson3/3. bastion/subnet.tf
+++ lesson3/3. bastion/subnet.tf
@@ -1,5 +1,6 @@
 variable "availability_zones" { type = list(string) }
 variable "subnet_public_cidr_blocks" { type = list(string) }
+variable "subnet_private_cidr_blocks" { type = list(string) }
 
 /**
  * subnet作成
@@ -13,5 +14,16 @@ resource "aws_subnet" "subnet_public" {
 
   tags = {
     Name = format("${var.user_name}-public-%01d-subnet", count.index + 1)
+  }
+}
+
+resource "aws_subnet" "subnet_private" {
+  count             = length(var.subnet_private_cidr_blocks)
+  vpc_id            = resource.aws_vpc.vpc.id
+  availability_zone = var.availability_zones[count.index % length(var.availability_zones)]
+  cidr_block        = var.subnet_private_cidr_blocks[count.index]
+
+  tags = {
+    Name = format("${var.user_name}-private-%01d-subnet", count.index + 1)
   }
 }
