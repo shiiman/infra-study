85f471d741ec28dc00c6e4c8b0efb5d6cf1e0974
shiiman <hsnonsense5@gmail.com>
2022-06-07 14:28:58 +0900

    add private instance

---
+++ lesson3/5. private_instance/route_table.tf
@@ -0,0 +1,26 @@
+/**
+ * Route Table作成
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
+ */
+resource "aws_route_table" "route_table_public" {
+  vpc_id = resource.aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = resource.aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "${var.user_name}-rt-public"
+  }
+}
+
+/**
+ * Route Table Association作成
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
+ */
+resource "aws_route_table_association" "route_table_association_public" {
+  count          = length(resource.aws_subnet.subnet_public.*.id)
+  subnet_id      = resource.aws_subnet.subnet_public.*.id[count.index]
+  route_table_id = resource.aws_route_table.route_table_public.id
+}
