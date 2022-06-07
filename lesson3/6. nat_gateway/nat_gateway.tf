5a2f0c761030b5f4aa623dcca1a2242b589fc831
shiiman <hsnonsense5@gmail.com>
2022-06-07 15:00:23 +0900

    add nat gateway

---
+++ lesson3/6. nat_gateway/nat_gateway.tf
@@ -0,0 +1,24 @@
+/**
+ * EIP作成
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
+ */
+resource "aws_eip" "eip" {
+  vpc = resource.aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.user_name}-ng-eip"
+  }
+}
+
+/**
+ * NAT Gateway作成
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/nat_gateway
+ */
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = resource.aws_eip.eip.id
+  subnet_id     = resource.aws_subnet.subnet_public.0.id
+
+  tags = {
+    Name = "${var.user_name}-ng"
+  }
+}
