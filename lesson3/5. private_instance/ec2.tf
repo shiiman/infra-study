6f8f2896108b3bae8426f92a4f9a357124eda9f8
shiiman <hsnonsense5@gmail.com>
2022-06-10 16:20:55 +0900

    indent

--- lesson3/5. private_instance/ec2.tf
+++ lesson3/5. private_instance/ec2.tf
@@ -3,10 +3,10 @@
  * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
  */
 resource "aws_instance" "bastion" {
-  count         = 1
-  ami           = "ami-02c3627b04781eada"
-  instance_type = "t2.micro"
-  key_name      = "instance_access_key"
+  count                       = 1
+  ami                         = "ami-02c3627b04781eada"
+  instance_type               = "t2.micro"
+  key_name                    = "instance_access_key"
   vpc_security_group_ids      = [resource.aws_security_group.sg_bastion.id]
   subnet_id                   = resource.aws_subnet.subnet_public.*.id[count.index % length(resource.aws_subnet.subnet_public.*.id)]
   associate_public_ip_address = true
@@ -21,10 +21,10 @@ resource "aws_instance" "bastion" {
 }
 
 resource "aws_instance" "private_instance" {
-  count         = 1
-  ami           = "ami-02c3627b04781eada"
-  instance_type = "t2.micro"
-  key_name      = "instance_access_key"
+  count                       = 1
+  ami                         = "ami-02c3627b04781eada"
+  instance_type               = "t2.micro"
+  key_name                    = "instance_access_key"
   vpc_security_group_ids      = [resource.aws_security_group.sg_private_instance.id]
   subnet_id                   = resource.aws_subnet.subnet_private.*.id[count.index % length(resource.aws_subnet.subnet_private.*.id)]
   associate_public_ip_address = false
