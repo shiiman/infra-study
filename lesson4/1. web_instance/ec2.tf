d0bff8fe85757ac1a7decbf81a2c6ed3cf47b859
shiiman <hsnonsense5@gmail.com>
2022-07-12 12:41:00 +0900

    update instance

--- lesson4/1. web_instance/ec2.tf
+++ lesson4/1. web_instance/ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "web_instance" {
   ami                         = "ami-02c3627b04781eada"
   instance_type               = "t2.micro"
   vpc_security_group_ids      = [resource.aws_security_group.sg_web_instance.id]
-  subnet_id                   = resource.aws_subnet.subnet_web.*.id[count.index % length(resource.aws_subnet.subnet_web.*.id)]
+  subnet_id                   = resource.aws_subnet.subnet_private.*.id[count.index % length(resource.aws_subnet.subnet_private.*.id)]
   associate_public_ip_address = false
 
   tags = {
