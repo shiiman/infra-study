c1be05710b9ae2b0bd6a9b4040838e461529d87e
shiiman <hsnonsense5@gmail.com>
2022-07-12 17:03:46 +0900

    add listener

--- lesson4/2. loadbalancer/lb.tf
+++ lesson4/2. loadbalancer/lb.tf
@@ -31,3 +31,18 @@ resource "aws_lb_target_group_attachment" "lb_target_group_attachment" {
   target_id        = resource.aws_instance.web_instance.*.id[count.index]
   port             = 80
 }
+
+/**
+ * lb listener作成
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_listener
+ */
+resource "aws_lb_listener" "lb_listener" {
+  load_balancer_arn = resource.aws_lb.application_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = resource.aws_lb_target_group.lb_target_group.arn
+  }
+}
