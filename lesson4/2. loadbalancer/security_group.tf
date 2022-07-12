872d74cd65593d67e0a47f7473d00dcb1043d4f0
shiiman <hsnonsense5@gmail.com>
2022-07-12 17:22:31 +0900

    add sg

--- lesson4/2. loadbalancer/security_group.tf
+++ lesson4/2. loadbalancer/security_group.tf
@@ -33,3 +33,25 @@ resource "aws_security_group_rule" "security_group_rule_egress_web_instance" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = resource.aws_security_group.sg_web_instance.id
 }
+
+# lbからwebインスタンスへのhttp
+resource "aws_security_group_rule" "security_group_rule_web_instance_from_lb_http" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = resource.aws_security_group.sg_web_instance.id
+  source_security_group_id = resource.aws_security_group.sg_lb.id
+}
+
+variable "company_ip" { type = list(string) }
+
+# 会社からlbへのhttp
+resource "aws_security_group_rule" "security_group_rule_lb_from_company_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = var.company_ip
+  security_group_id = resource.aws_security_group.sg_lb.id
+}
--- lesson4/2. loadbalancer/terraform.tfvars
+++ lesson4/2. loadbalancer/terraform.tfvars
@@ -6,3 +6,8 @@ subnet_public_cidr_blocks  = ["172.16.0.0/24", "172.16.1.0/24"]
 subnet_private_cidr_blocks = ["172.16.10.0/24", "172.16.11.0/24"]
 
 iam_instance_profile = "AmazonEC2RoleforSSM"
+
+// 社内IP
+company_ip = [
+  // TODO: 会社のIP
+]
