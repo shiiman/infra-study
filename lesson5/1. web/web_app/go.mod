2fa2bc186509ecaffa03bd660445baf07d3ef811
shiiman <hsnonsense5@gmail.com>
2022-07-15 18:02:04 +0900

    add lesson5

---
+++ lesson5/1. web/web_app/go.mod
@@ -0,0 +1,5 @@
+module web_app
+
+go 1.18
+
+require github.com/gomodule/redigo v1.8.9
--- lesson5/1. web/web_app/main.go
+++ lesson5/1. web/web_app/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+const (
+	DB_USER = "root"
+	DB_PASS = "root"
+	DB_HOST = "127.0.0.1"
+	DB_PORT = "3306"
+	DB_NAME = "test_db"
+
+	CACHE_HOST = "127.0.0.1"
+	CACHE_PORT = "6379"
 )
 
 func main() {
@@ -13,5 +27,22 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	hostname, _ := os.Hostname()
-	fmt.Fprintf(w, "Hello, Infra Study\nhostname: "+hostname+"\n")
+
+	dbStat := "成功"
+	dbconf := DB_USER + ":" + DB_PASS + "@tcp(" + DB_HOST + ":" + DB_PORT + ")/" + DB_NAME + "?charset=utf8mb4"
+	db, err := sql.Open("mysql", dbconf)
+	if err != nil {
+		dbStat = "失敗"
+	}
+	defer db.Close()
+
+	cacheStat := "成功"
+	cacheconf := CACHE_HOST + ":" + CACHE_PORT
+	cache, err := redis.Dial("tcp", cacheconf)
+	if err != nil {
+		panic(err)
+	}
+	defer cache.Close()
+
+	fmt.Fprintf(w, "Hello, Infra Study\nhostname: "+hostname+"\nDB接続"+dbStat+"\nCache接続"+cacheStat+"\n")
 }
