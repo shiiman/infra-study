9243fd885dd7b8ab974f42204d1d548cc3f771b0
shiiman <hsnonsense5@gmail.com>
2022-07-20 17:15:55 +0900

    add syukudai1

--- lesson5/4. syukudai1/cache.tf
+++ lesson5/4. syukudai1/cache.tf
@@ -38,7 +38,7 @@ resource "aws_elasticache_replication_group" "aws_elasticache_replication_group"
   engine                        = "redis"
   engine_version                = "5.0.6"
   port                          = "6379"
-  num_cache_clusters            = "2"
+  number_cache_clusters         = "2"
   automatic_failover_enabled    = true
   subnet_group_name             = resource.aws_elasticache_subnet_group.elasticache_subnet_group.name
   parameter_group_name          = resource.aws_elasticache_parameter_group.parameter_group.id
