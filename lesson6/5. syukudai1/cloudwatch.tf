cab4f2b38122cb3c0f3cfc6523643d937e1e07c5
shiiman <hsnonsense5@gmail.com>
2022-08-01 18:25:11 +0900

    add syukudai1

--- lesson6/5. syukudai1/cloudwatch.tf
+++ lesson6/5. syukudai1/cloudwatch.tf
@@ -3,5 +3,5 @@
  * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_group
  */
 resource "aws_cloudwatch_log_group" "cloud_watch_log_group" {
-  name = "/aws/ecs/${var.user_name}/app"
+  name = "/aws/ecs/${var.user_name}"
 }
