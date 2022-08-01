38b178f24d71930086c5e33bfb79bf462899c145
shiiman <hsnonsense5@gmail.com>
2022-08-01 18:12:12 +0900

    add syukudai1

--- lesson6/5. syukudai1/ecs.tf
+++ lesson6/5. syukudai1/ecs.tf
@@ -34,7 +34,8 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
       "logDriver": "awslogs",
       "options": {
         "awslogs-group": "${resource.aws_cloudwatch_log_group.cloud_watch_log_group}",
-        "awslogs-region": "ap-northeast-1"
+        "awslogs-region": "ap-northeast-1",
+        "awslogs-stream-prefix": ""
       }
     },
     "portMappings": [
