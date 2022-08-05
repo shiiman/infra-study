e93e7591422fc7019077833eb7d0d7b493759df1
shiiman <hsnonsense5@gmail.com>
2022-08-05 12:35:20 +0900

    cloudfront

---
+++ lesson7/2. cloudfront/s3.tf
@@ -0,0 +1,16 @@
+/**
+ * バケット作成
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+ */
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.user_name}-bucket"
+}
+
+/**
+ * バケットACL作成
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_acl
+ */
+resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+  bucket = resource.aws_s3_bucket.bucket.id
+  acl    = "private"
+}
