17d79c15f404f6d647cccfe8774e2c57d4bea129
shiiman <hsnonsense5@gmail.com>
2022-08-05 17:01:44 +0900

    add syukudai1

---
+++ lesson7/5. syukudai1/s3.tf
@@ -0,0 +1,54 @@
+/**
+ * バケット作成
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+ */
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.user_name}-bucket"
+}
+
+/**
+ * バケットACL作成
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_acl
+ */
+resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+  bucket = resource.aws_s3_bucket.bucket.id
+  acl    = "private"
+}
+
+/**
+ * ポリシードキュメント作成
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document
+ */
+data "aws_iam_policy_document" "iam_policy_document" {
+  statement {
+    sid       = "CloudFrontOriginAccessIdentity"
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["${resource.aws_s3_bucket.bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [resource.aws_cloudfront_origin_access_identity.cloudfront_origin_access_identity.iam_arn]
+    }
+  }
+}
+
+/**
+ * バケットポリシー作成
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_policy
+ */
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = resource.aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.iam_policy_document.json
+}
+
+/**
+ * バージョニング設定
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_versioning
+ */
+resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
+  bucket = resource.aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
