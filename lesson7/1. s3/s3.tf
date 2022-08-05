c19a80d1f54ee621563d4a8572eb6c06d0ee89df
shiiman <hsnonsense5@gmail.com>
2022-08-05 11:41:25 +0900

    lesson7

--- lesson7/1. s3/s3.tf
+++ lesson7/1. s3/s3.tf
@@ -6,6 +6,10 @@ resource "aws_s3_bucket" "bucket" {
   bucket = "${var.user_name}-bucket"
 }
 
+/**
+ * バケットACL作成
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_acl
+ */
 resource "aws_s3_bucket_acl" "s3_bucket_acl" {
   bucket = resource.aws_s3_bucket.bucket.id
   acl    = "private"
