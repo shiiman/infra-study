85ae42694511fb7a64084d7c353e1a3958cda3c4
shiiman <hsnonsense5@gmail.com>
2022-08-05 15:02:32 +0900

    update cloud front

--- lesson7/2. cloudfront/s3.tf
+++ lesson7/2. cloudfront/s3.tf
@@ -14,3 +14,30 @@ resource "aws_s3_bucket_acl" "s3_bucket_acl" {
   bucket = resource.aws_s3_bucket.bucket.id
   acl    = "private"
 }
+
+/**
+ * ポリシードキュメント作成
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document
+ */
+data "aws_iam_policy_document" "iam_policy_document" {
+  statement {
+    sid       = "CloudFrontOriginAccessIdentity"
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["${resource.aws_s3_bucket.bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [resource.aws_cloudfront_origin_access_identity.cloudfront_origin_access_identity.arn]
+    }
+  }
+}
+
+/**
+ * バケットポリシー作成
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_policy
+ */
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = resource.aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.iam_policy_document.json
+}
