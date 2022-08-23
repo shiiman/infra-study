e0e7800df66d2efc5d23a1f0362c0192df9657b6
shiiman <hsnonsense5@gmail.com>
2022-08-23 12:03:09 +0900

    add lesson8 before

---
+++ lesson8/1. code_commit/common.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-northeast-1"
+}
+
+variable "user_name" {}
